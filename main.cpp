4354d7e5405bf3c789a35413ee8f3526e1b28db4
kotlinx <2353829347wdf@gmail.com>
2025-12-24 23:07:24 +0800

    rust deepfliter so
    Signed-off-by: kotlinx <2353829347wdf@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include "AAudioRecorder.h"
+#include "df.h"
 
 #include <thread>
 #include <chrono>
 
 int main() {
 
-    CallbackPCMRecorder recorder;
+    // CallbackPCMRecorder recorder;
+    //
+    // bool startResult = recorder.start("/sdcard/source.pcm", "/sdcard/record.pcm");
+    //
+    // LOGI("start aaudio recorder result is: %d", startResult);
+    //
+    // std::this_thread::sleep_for(std::chrono::seconds(20));
+    //
+    // recorder.stop();
 
-    bool startResult = recorder.start("/sdcard/source.pcm", "/sdcard/record.pcm");
+    DFState *state = df_create("/sdcard/DeepFilterNet2_onnx_ll.tar.gz", 20, "info");
 
-    LOGI("start aaudio recorder result is: %d", startResult);
-    
-    std::this_thread::sleep_for(std::chrono::seconds(20));
-
-    recorder.stop();
+    if (state != nullptr) {
+        LOGI("df_create is not null");
+    } else {
+        LOGI("df_create is null");
+    }
 
     return 0;
 }
